34f0ac89b139dbecd462d4b343574898ff1f0f52
Nate Varghese <nvarghese@nvcomputers.com>
2018-07-04 21:44:09 -0700

    not sensitive

---
+++ Slink/NotSensitive.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Slink
+{
+    public static class NotSensitive
+    {
+        public class SlinkKeys
+        {
+            public static string clientid_key = "Slink-mobile";
+            public static string one_signal_app_id = "a49ee6d2-0ad7-44a5-9345-d99b257adbec";
+            public static string facebook_app_id = "282730945236494";
+            public static string facebook_display_name = "Slink";
+            public static string twitter_consumer_key = "om4KaPat3IdmyE3nKjfK33veU";
+            public static string twitter_consumer_secret = "TpX1KBfgAts1NYlQJQdjGIAFZZ1eeFiiEjKxvTRABg0Bwww98n";
+            public static string linkedin_client_id = "864o7d7uh6idk0";
+            public static string linkedin_client_secret = "zPip4X2YsoCR75NV";
+            public static string instagram_client_id = "2f7dd79174c943c09385ea17b45655a2";
+            public static string instagram_client_secret = "90cb23212b0f4ca3a4a21a6498d013f8";
+            public static string pinterest_client_id = "4912850120187324703";
+            public static string pinterest_client_secret = "2aa821343e217a3fbbdadbc0211fde6ebccbd41bbe730e04609d123d2cb45c47";
+            public static string google_plus_api_key = "AIzaSyChBOlCe9nmXBIRU9jQ0Z47rzdXOtXFaSs";
+            public static string github_client_id = "a331b6cc9ea2dde27c20";
+            public static string github_client_secret = "e26391277ed64bacc8e46b37abbd9f00284ec781";
+        }
+
+        public class SystemUrls
+        {
+            public static string realm_server_ip = "http://realm-2129060701.us-east-2.elb.amazonaws.com:9080";
+            public static string base_url = "http://api-2090217776.us-east-2.elb.amazonaws.com/";
+            public static string s3_url = "https://s3.us-east-2.amazonaws.com/slinka/";
+            public static string linkedin_redirect_url = "https://mobile.linkedin.com";
+            public static string instagram_redirect_url = "https://www.instagram.com";
+            public static string pinterest_redirect_url = "https://www.pinterest.com";
+            public static string github_redirect_url = "https://github.com";
+            public static string privacy_policy = "http://nvcomputers.com/privacyPolicy/";
+        }
+
+    }
+}
