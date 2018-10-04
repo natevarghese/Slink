1fe3d3938761a01c12b16911803f931a47218db3
Ekamdeep Kaur <kaurekamdeep@seasiainfotech.com>
2018-10-04 22:35:24 +0530

    Design change reciever

---
+++ Droid/Services/DesignChangedReciever.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.Content;
+using Com.OneSignal.Abstractions;
+using static Slink.Droid.ActionBroadcastReceiver;
+
+namespace Slink.Droid.Services
+{
+    public class DesignChangedReciever : BroadcastReceiver
+    {
+        private IOnDataRecievedListener mListener;
+        public Action<Intent> NotificationReceived;
+
+        public DesignChangedReciever(IOnDataRecievedListener listener)
+        {
+            mListener = listener;
+            mListener.RecieveData();
+        }
+
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            NotificationReceived?.Invoke(intent);
+            var title = intent.GetStringExtra(Strings.InternalNotifications.notification_design_changed);
+            //  if (String.IsNullOrEmpty(title)) return;
+            if (mListener != null)
+                mListener.RecieveData();
+
+        }
+
+    }
+}
