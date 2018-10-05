6b9ba1843116a78787bfba489eb05f12925b3680
Ekamdeep Kaur <kaurekamdeep@seasiainfotech.com>
2018-10-05 10:53:45 +0530

    "commited"

---
+++ Droid/Fragments/ConnectionFragment.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Slink.Droid.Fragments
+{
+    public class ConnectionFragment : BaseFragment
+    {
+        protected RelativeLayout BackgroundView;
+        protected RelativeLayout relative_Connection;
+        protected TextView text_Connction;
+
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.Connection, container, false);
+
+            relative_Connection = (RelativeLayout)view.FindViewById(Resource.Id.relativeShare);
+            text_Connction = (TextView)view.FindViewById(Resource.Id.textConnection);
+            BackgroundView = (RelativeLayout)view.FindViewById(Resource.Id.BackgroundView);
+
+
+            return view;
+        }
+    }
+}
---
+++ Droid/Fragments/DiscoverFragment.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Views.Animations;
+using Android.Widget;
+
+namespace Slink.Droid.Fragments
+{
+    public class DiscoverFragment : BaseFragment
+    {
+        protected RelativeLayout BackgroundView;
+        protected RelativeLayout relative_Discover;
+
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+
+        {
+
+            var view = inflater.Inflate(Resource.Layout.Discover, container, false);
+
+            BackgroundView = (RelativeLayout)view.FindViewById(Resource.Id.BackgroundView);
+            relative_Discover = (RelativeLayout)view.FindViewById(Resource.Id.relativeDiscover);
+            
+           
+
+            // Animation Of vibrating ring of whole circle//
+            var anim = AnimationUtils.LoadAnimation(Context,Resource.Animator.scaleanim);
+            relative_Discover.StartAnimation(anim);
+            return view;
+        }
+    }
+}
